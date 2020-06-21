cfaccb50234168e856772c6547e9a93304693e7f
Kridsada-Wannasing <kridsada_aex002@hotmail.com>
2020-06-21 12:29:29 +0700

    validate create data form

--- src/components/CreateDataDialog.vue
+++ src/components/CreateDataDialog.vue
@@ -4,73 +4,97 @@
       <v-card-title>
         <span class="headline">Add Data</span>
       </v-card-title>
-      <v-card-text>
-        <v-container>
-          <v-row>
-            <v-col cols="12" md="6">
-              <v-text-field
-                label="first_name"
-                v-model="newData.first_name"
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12" md="6">
-              <v-text-field
-                label="last_name"
-                v-model="newData.last_name"
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12" md="6">
-              <v-text-field label="age" v-model="newData.age"></v-text-field>
-            </v-col>
-            <v-col cols="12" md="6">
-              <v-radio-group v-model="newData.sex" :mandatory="false">
-                <v-row>
-                  <v-col cols="3"><p>sex</p></v-col>
-                  <v-col cols="4">
-                    <v-radio label="Male" value="M"></v-radio>
-                  </v-col>
-                  <v-col cols="4"
-                    ><v-radio label="Femail" value="F"></v-radio
-                  ></v-col>
-                </v-row>
-              </v-radio-group>
-            </v-col>
-            <v-col cols="12" md="6">
-              <v-text-field
-                label="email"
-                v-model="newData.email"
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12" md="6">
-              <v-text-field label="tel" v-model="newData.tel"></v-text-field>
-            </v-col>
-            <v-col cols="12">
-              <v-select
-                label="status"
-                :items="items"
-                v-model="newData.status"
-              ></v-select>
-            </v-col>
-            <v-col cols="12">
-              <v-textarea label="remark" v-model="newData.remark"></v-textarea>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-card-text>
-      <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn color="blue darken-1" text @click="cancel">Cancel</v-btn>
-        <v-btn color="blue darken-1" text @click="create">Create</v-btn>
-      </v-card-actions>
+      <v-form ref="form" v-model="valid">
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="12" md="6">
+                <v-text-field
+                  label="first_name"
+                  v-model="newData.first_name"
+                  :rules="firstNameRules"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" md="6">
+                <v-text-field
+                  label="last_name"
+                  v-model="newData.last_name"
+                  :rules="lastNameRules"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" md="6">
+                <v-text-field
+                  label="age"
+                  v-model="newData.age"
+                  :rules="ageRules"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" md="6">
+                <v-radio-group
+                  v-model="newData.sex"
+                  :rules="sexRules"
+                  :mandatory="false"
+                >
+                  <v-row>
+                    <v-col cols="3"><p>sex</p></v-col>
+                    <v-col cols="4">
+                      <v-radio label="Male" value="M"></v-radio>
+                    </v-col>
+                    <v-col cols="4"
+                      ><v-radio label="Femail" value="F"></v-radio
+                    ></v-col>
+                  </v-row>
+                </v-radio-group>
+              </v-col>
+              <v-col cols="12" md="6">
+                <v-text-field
+                  label="email"
+                  v-model="newData.email"
+                  :rules="emailRules"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" md="6">
+                <v-text-field
+                  label="tel"
+                  v-model="newData.tel"
+                  :rules="telRules"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-select
+                  label="status"
+                  :items="items"
+                  v-model="newData.status"
+                  :rules="statusRules"
+                ></v-select>
+              </v-col>
+              <v-col cols="12">
+                <v-textarea
+                  label="remark"
+                  v-model="newData.remark"
+                ></v-textarea>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="cancel">Cancel</v-btn>
+          <v-btn color="blue darken-1" text @click="create">Create</v-btn>
+        </v-card-actions>
+      </v-form>
     </v-card>
   </v-dialog>
 </template>
 
 <script>
+import { formValidateMixins } from "@/mixins/formValidateMixins";
 export default {
+  mixins: [formValidateMixins],
   data() {
     return {
       items: ["active", "deactive"],
+      valid: true,
       newData: {
         first_name: "",
         last_name: "",
@@ -85,8 +109,10 @@ export default {
   },
   methods: {
     create() {
-      this.$store.dispatch("table/createDataTable", this.newData);
-      this.dataDialog = false;
+      if (this.$refs.form.validate()) {
+        this.$store.dispatch("table/createDataTable", this.newData);
+        this.dataDialog = false;
+      }
     },
     cancel() {
       this.dataDialog = false;
